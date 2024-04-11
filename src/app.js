e7e39f664c3cb78732b9296f0fd50adcca1ec1b8
George <cortes.george93@icloud.com>
2024-04-10 21:30:00 -0700

    Added weather png

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 let key = "cd86a0af9e4c4235b44223125240904"
 let val ="Hayward, Ca" 
+let date = new Date()
+let daysofWeek= ["Sunday", "Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+let day =  document.getElementById("dayOfWeek")
+day.innerText = daysofWeek[date.getDay()]
+console.log(dayOfWeek)
 input = document.getElementById("search").value
+weatherImagesUrl = {
+    "Sunny":"./assets/sunny-3.png",
+    "Partly cloudy":"./assets/weather/64x64/day/116.png",
+    "Cloudy": "./assets/weather/64x64/day/119.png",
+    "Raining": "./assets/weather/64x64/day/266.png"
+}
 let data = ""
 async function getWeather(){
     let jsonResponse = await fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=${val}`, {mode:"cors"})
@@ -9,20 +20,24 @@ async function getWeather(){
 }
 getWeather().then((x)=>{
     data = x
-let loc = document.getElementById("loc")
-loc.innerText = data.location.name + ", " + data.location.region
-let temp = document.getElementById("temp")
-temp.innerText = data.current.temp_f // farenheight
-let conditionIcon = document.getElementById("conditionIcon")
-conditionIcon.src = data.current.condition.icon
-let feelsLike = document.getElementById("feelsLike")
-feelsLike.innerText = data.current.condition.text
-let windSpeed = document.getElementById("windSpeed")
-windSpeed.innerText =  data.current.wind_mph + " mph"
-let humidity = document.getElementById("humidity")
-humidity.innerText = data.current.humidity + " %"
-let uvIndex = document.getElementById("uvIndex")
-uvIndex.innerText = data.current.uv 
-let pressure = document.getElementById("pressure")
-pressure.innerText = data.current.pressure_mb + " mb"
+    let currTime = document.getElementById("time")
+    currTime.innerText = data.current.last_updated
+    let loc = document.getElementById("loc")
+    loc.innerText = data.location.name + ", " + data.location.region
+    let temp = document.getElementById("temp")
+    temp.innerText = "+" + data.current.temp_f +"°F" // farenheight
+    let conditionIcon = document.getElementById("conditionIcon")
+    console.log(data.current.condition.text)
+    console.log(weatherImagesUrl[data.current.condition.text])
+    conditionIcon.src = weatherImagesUrl[data.current.condition.text]
+    let feelsLike = document.getElementById("feelsLike")
+    feelsLike.innerText = data.current.condition.text
+    let windSpeed = document.getElementById("windSpeed")
+    windSpeed.innerText =  data.current.wind_mph + " mph"
+    let humidity = document.getElementById("humidity")
+    humidity.innerText = data.current.humidity + " %"
+    let uvIndex = document.getElementById("uvIndex")
+    uvIndex.innerText = data.current.uv 
+    let pressure = document.getElementById("pressure")
+    pressure.innerText = data.current.pressure_mb + " mb"
 })
